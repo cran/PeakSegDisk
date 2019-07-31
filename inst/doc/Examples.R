bf8ba48bdc5aaf072fddb7f4e2714497cbaf58f6
Toby Dylan Hocking <toby.hocking@r-project.org>
2019-07-31 20:50:08 +0000

    version 2019.7.29

---
+++ inst/doc/Examples.R
@@ -0,0 +1,126 @@
+## ----setup, echo=FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  fig.width = 7,
+  fig.height = 3,
+  fig.align = "center",
+  comment = "#>"
+)
+
+## ------------------------------------------------------------------------
+sim.seg <- function(seg.mean, size.mean=15){
+  seg.size <- rpois(1, size.mean)
+  rpois(seg.size, seg.mean)
+}
+set.seed(1)
+seg.mean.vec <- c(1.5, 3.5, 0.5, 4.5, 2.5)
+z.list <- lapply(seg.mean.vec, sim.seg)
+(z.rep.vec <- unlist(z.list))
+
+## ----ggcount-------------------------------------------------------------
+count.df <- data.frame(
+  chrom="chrUnknown",
+  chromStart=0:(length(z.rep.vec)-1),
+  chromEnd=1:length(z.rep.vec),
+  count=z.rep.vec)
+library(ggplot2)
+gg.count <- ggplot()+
+  xlab("position")+
+  geom_point(aes(
+    chromEnd, count),
+    shape=1,
+    data=count.df)
+gg.count
+
+## ------------------------------------------------------------------------
+n.segs <- length(seg.mean.vec)
+seg.size.vec <- sapply(z.list, length)
+seg.end.vec <- cumsum(seg.size.vec)
+change.vec <- seg.end.vec[-n.segs]+0.5
+change.df <- data.frame(
+  changepoint=change.vec)
+gg.change <- gg.count+
+  geom_vline(aes(
+    xintercept=changepoint, color=model),
+    data=data.frame(change.df, model="simulation"))+
+  scale_color_manual(
+    values=c(
+      simulation="black",
+      fitted="green"))
+gg.change
+
+## ------------------------------------------------------------------------
+fit <- list()
+(fit$vec <- PeakSegDisk::PeakSegFPOP_vec(z.rep.vec, 10.5))
+
+## ------------------------------------------------------------------------
+gg.change+
+  geom_segment(aes(
+    chromStart+0.5, mean, xend=chromEnd+0.5, yend=mean, color=model),
+    data=data.frame(fit$vec$segments, model="fitted"))
+
+## ------------------------------------------------------------------------
+plot(fit$vec)
+
+## ------------------------------------------------------------------------
+(fit$df <- PeakSegDisk::PeakSegFPOP_df(count.df, 10.5))
+
+## ------------------------------------------------------------------------
+z.rle.vec <- rle(z.rep.vec)
+chromEnd <- cumsum(z.rle.vec$lengths)
+rle.df <- data.frame(
+  chrom="chrUnknown",
+  chromStart=c(0L, chromEnd[-length(chromEnd)]),
+  chromEnd,
+  count=z.rle.vec$values)
+gg.rle <- ggplot()+
+  geom_segment(aes(
+    chromStart+0.5, count, xend=chromEnd+0.5, yend=count),
+    data=rle.df)+
+  geom_point(aes(
+    chromEnd, count),
+    shape=1,
+    data=rle.df)+
+  geom_vline(aes(
+    xintercept=changepoint, color=model),
+    data=data.frame(change.df, model="simulation"))+
+  scale_color_manual(
+    values=c(
+      simulation="black",
+      fitted="green"))+
+  xlab("position")
+gg.rle
+
+## ------------------------------------------------------------------------
+(fit$rle <- PeakSegDisk::PeakSegFPOP_df(rle.df, 10.5))
+gg.rle+
+  geom_segment(aes(
+    chromStart+0.5, mean, xend=chromEnd+0.5, yend=mean, color=model),
+    data=data.frame(fit$rle$segments, model="fitted"))
+
+## ------------------------------------------------------------------------
+data.dir <- file.path(
+  tempfile(),
+  with(rle.df, sprintf(
+    "%s-%d-%d", chrom[1], min(chromStart), max(chromEnd))))
+dir.create(data.dir, showWarnings=FALSE, recursive=TRUE)
+coverage.bedGraph <- file.path(data.dir, "coverage.bedGraph")
+write.table(
+  rle.df, coverage.bedGraph,
+  sep="\t", row.names=FALSE, col.names=FALSE)
+
+## ------------------------------------------------------------------------
+(fit$dir <- PeakSegDisk::PeakSegFPOP_dir(data.dir, 10.5))
+
+## ------------------------------------------------------------------------
+if(interactive() && requireNamespace("future"))future::plan("multiprocess")
+(fit$search <- PeakSegDisk::sequentialSearch_dir(data.dir, 2L, verbose=1))
+
+## ------------------------------------------------------------------------
+lossDF <- function(L)data.frame(L$loss)[, names(fit$dir$loss)]
+do.call(rbind, lapply(fit, lossDF))
+
+## ------------------------------------------------------------------------
+four.peaks <- PeakSegDisk::sequentialSearch_dir(data.dir, 4L)
+four.peaks$others[, .(iteration, penalty, peaks)]
+
---
+++ inst/doc/Worst_case.R
@@ -0,0 +1,75 @@
+## ----setup, include = FALSE-----------------------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+options(width=100)
+
+## -------------------------------------------------------------------------------------------------
+data(Mono27ac, package="PeakSegDisk", envir=environment())
+library(data.table)
+loss.list <- list()
+N.data.vec <- 10^seq(1, 3)
+for(penalty in c(0, 1e2, 1e4, 1e6)){
+  for(N.data in N.data.vec){
+    some.cov <- Mono27ac$coverage[1:N.data]
+    some.inc <- data.table(some.cov)
+    some.inc[, count := 1:.N]
+    data.list <- list(
+      real=some.cov,
+      synthetic=some.inc)
+    for(short.type in names(data.list)){
+      df <- data.list[[short.type]]
+      fit <- PeakSegDisk::PeakSegFPOP_df(df, penalty)
+      loss.list[[paste(penalty, N.data, short.type)]] <- data.table(
+        N.data,
+        short.type,
+        fit$loss)
+    }
+  }
+}
+(loss <- do.call(rbind, loss.list))[, .(
+  penalty, short.type, N.data, 
+  mean.intervals, max.intervals,
+  megabytes, seconds)][order(penalty, short.type, N.data)]
+
+## -------------------------------------------------------------------------------------------------
+(worst.dt <- data.table(
+  N.data=N.data.vec,
+  mean.intervals=(N.data.vec+1)/2,
+  short.type="theoretical"))
+
+## ---- fig.height=3--------------------------------------------------------------------------------
+library(ggplot2)
+one <- function(short.type, data.type, color){
+  data.table(short.type, data.type, color)
+}
+type.dt <- rbind(
+  one("theoretical", "Theoretical\nworst case", "grey"),
+  one("synthetic", "Synthetic\nIncreasing", "red"),
+  one("real", "Real ChIP-seq", "black"))
+loss.types <- type.dt[loss, on=list(short.type)]
+worst.types <- type.dt[worst.dt, on=list(short.type)]
+(type.colors <- type.dt[, structure(color, names=data.type)])
+ggplot()+
+  guides(
+    color=guide_legend(keyheight=3)
+  )+
+  geom_blank(aes(
+    N.data, 1),
+    data=data.table(N.data=c(5, 2000)))+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0, "lines"))+
+  facet_grid(. ~ penalty, labeller=label_both)+
+  geom_line(aes(
+    N.data, mean.intervals, color=data.type),
+    data=worst.types)+
+  scale_color_manual(
+    values=type.colors,
+    breaks=names(type.colors))+
+  geom_line(aes(
+    bedGraph.lines, mean.intervals, color=data.type),
+    data=loss.types)+
+  scale_x_log10("N data")+
+  scale_y_log10("Mean intervals (candidate changepoints)")
+
